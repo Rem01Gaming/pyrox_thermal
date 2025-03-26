bed856de55281216ff8283eddb3497219f7d4f70
Rem01Gaming <Rem01_Gaming@proton.me>
2025-03-26 13:29:57 +0700

    Remove license file on installation
    Signed-off-by: Rem01Gaming <Rem01_Gaming@proton.me>

--- customize.sh
+++ customize.sh
@@ -3,3 +3,5 @@ ui_print " Overheat and fire ahead"
 ui_print " Developer doesn't account for any damages"
 ui_print " from using this module, use it on your own risk."
 ui_print "******************************"
+
+rm "$MODPATH/LICENSE"
